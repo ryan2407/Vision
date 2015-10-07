d6182a2eec5f2891e300a73094c816e4e925c1b9
Ryan Murray <ryanmurrayemail@gmail.com>
2015-10-07 16:16:40 +1000

    updated htaccess and db file

--- wp-config.php
+++ wp-config.php
@@ -16,16 +16,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'visiondev');
+define('DB_NAME', 'vision');
 
 /** MySQL database username */
-define('DB_USER', 'devuser');
+define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'QgxllEivF56enEGdC2KW');
+define('DB_PASSWORD', '136pedro');
 
 /** MySQL hostname */
-define('DB_HOST', 'db.vision.org.au');
+define('DB_HOST', 'localhost');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');

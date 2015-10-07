99bb0f9e9b841de4fa04f9dd869199d2792558f6
Ryan Murray <ryanmurrayemail@gmail.com>
2015-10-07 16:18:21 +1000

    updated base root

--- wp-config.php
+++ wp-config.php
@@ -73,7 +73,7 @@ define('WP_DEBUG', true);
 define( 'WP_ALLOW_MULTISITE', true );
 define('MULTISITE', true);
 define('SUBDOMAIN_INSTALL', false);
-define('DOMAIN_CURRENT_SITE', 'dev.vision.org.au');
+define('DOMAIN_CURRENT_SITE', '159.203.93.119');
 define('PATH_CURRENT_SITE', '/');
 define('SITE_ID_CURRENT_SITE', 1);
 define('BLOG_ID_CURRENT_SITE', 1);
